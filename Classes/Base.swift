613c7677d142df2a0ab98c066f0c982678f82619
KG Haarish Kannan <haarish@ncompass.in>
2022-03-14 15:56:30 +0530

    updated podpsec

---
+++ Classes/Base.swift
@@ -0,0 +1,16 @@
+//
+//  Base.swift
+//  NCiOSPod
+//
+//  Created by Haarish on 02/03/22.
+//
+
+import Foundation
+
+public class SimpleClass {
+    let simpleString = "Hello World"
+    
+    func log() {
+        print(simpleString)
+    }
+}
---
+++ Example/Pods/Target Support Files/NCiOSPod/NCiOSPod-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_NCiOSPod : NSObject
+@end
+@implementation PodsDummy_NCiOSPod
+@end
